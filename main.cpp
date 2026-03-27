6d32bd103b8d173b01537c5f2cacdbf288665ef5
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-03-27 20:18:43 -0300

    std,cout

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include<iostream>
-#include<iomanip>
+
 
 int main(){
 
-    int a = 8; 
-     //int numero inteiro
-    float b = 9.6;
-    //float numero de ponto flutuante ocupa 4bytes.
-    double c = 8.6952132545;
-    //double ocupa 8bytes.
-
-    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
-
-    std::cout<<a<<std::endl;
-    std::cout<<b<<std::endl;
-    std::cout<<c<<std::endl;
+    std::cout<<8+2<<std::endl;
+    std::cout<<10-4<<std::endl;
+    std::cout<<4*3<<std::endl;
+    std::cout<<8/2<<std::endl;
 
     return 0;
 }
