45da30939c643ee0b69fe20354a7afc0aa8b0cc8
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-03-27 21:30:40 -0300

    cout,cin

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 
 int main(){
 
-   int idade = 16;
-   float altura = 1.65;
-   std::string nome = "Yasmim";
+std::string nome;
 
+std::cout<<"Qual seu nome: "<<std::endl;
+std::cin>>nome;
 
-    std::cout<<"Meu nome é "<<nome<<"."<<std::endl;
-    std::cout<<"Minha altura é "<<altura<<"m."<<std::endl;
-    std::cout<<"Minha idade é "<<idade<<" anos."<<std::endl;
+std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+
 
 
     return 0;
