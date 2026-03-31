8f2a5d34d4101a12be56f315b7ea4a9a15620771
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-03-30 22:01:21 -0300

    || opção

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int a = 10;
 int b = 6;
 
 std::cout<<((a > b) && (a + b) >= 16)<<std::endl;
-
+// && é uma necessidade 
+std::cout<<((a > b) || (a + b) >= 16)<<std::endl;
+// || é uma opção
 
     return 0;
 }
