5fd3920705c92b49d45578d899aae19a8d5b715e
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-01 19:39:07 -0300

    char letra

--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 
 int main(){
 
-bool condicao = true;
-//bool é usado para condição, ou seja, verdadeiro ou falso
+char letra = 'E';
 
-bool condicao1 = false;
+std::cout<<letra<<std::endl;
 
-std::cout<<condicao<<std::endl;
-std::cout<<condicao1<<std::endl;
+
 
     return 0;
 }
