8af496838b5ef634f90a9882cde30654d8ba415c
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-01 19:46:32 -0300

    auto

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 
 int main(){
 
-char letra = 'E';
+auto num1 = 10;
+auto num2 = 15.80f;
+auto letra = 'Y';
+auto texto = "Yasmim";
 
+std::cout<<num1<<std::endl;
+std::cout<<num2<<std::endl;
 std::cout<<letra<<std::endl;
-
-
+std::cout<<texto<<std::endl;
 
     return 0;
 }
