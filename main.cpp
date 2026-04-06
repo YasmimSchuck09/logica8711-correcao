31f2bd9e87bdbe7ba7d5814dc1011407ccd68e98
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-06 20:34:43 -0300

    string cálculos

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-std::string nome = "Yasmim";
-std::string sobrenome = "Schuck";
+int a = 40;
+int b = 10;
+std::string c = "40";
+std::string d = "10";
 
-std::cout<<nome + " " + sobrenome<<std::endl;
+std::cout<<a+b<<std::endl;
+std::cout<<c+d<<std::endl;
 
       return 0;
 }
