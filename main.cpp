99513ff07ee43255ab5a9b545b9efda1829c517e
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-06 21:08:03 -0300

    int  e string

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
+#include<string>
 
 int main(){
 
-int a = 40;
-int b = 10;
-std::string c = "40";
-std::string d = "10";
+      int a = 40;
+      std::string d = "10";
 
-std::cout<<a+b<<std::endl;
-std::cout<<c+d<<std::endl;
+      std::cout<<a+d<<std::endl; 
 
       return 0;
 }
