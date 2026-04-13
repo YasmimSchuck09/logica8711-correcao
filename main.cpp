cf9428183edacbea981dde21e106d9ef220ed15f
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-13 19:17:26 -0300

    if, else- teste idade

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
 #include<iostream>
-#include<iostream>
 
 int main(){
 
-std::string user;
-std::string senha;
+int idade;
 
-std::cout<<"Digite seu usuario de acesso: "<<std::endl;
-std::cin>>user;
+std::cout<<"Digite a sua idade: "<<std::endl;
+std::cin>>idade;
 
-std::cout<<"Digite sua senha: "<<std::endl;
-std::cin>>senha;
-
-if(senha == "123456789"){
-std::cout<<"O usuario "<<user<<" está logado com sucesso!!"<<std::endl;
+if(idade >= 18){
+      std::cout<<"Voce é maior de idade!"<<std::endl;
 }else{
-      std::cout<<"Usuário ou senha inválidos!"<<std::endl;
+      std::cout<<"Voce é menor de idade!!"<<std::endl;
 }
-
 
       return 0;
 }
