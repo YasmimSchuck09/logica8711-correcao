ea1c1d2fc4065aa0cfb0570988d43aa53b838826
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-13 19:46:01 -0300

    utilização do (else if) relacionado a idades

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@ int idade;
 std::cout<<"Digite a sua idade: "<<std::endl;
 std::cin>>idade;
 
-if(idade >= 18){
-      std::cout<<"Voce é maior de idade!"<<std::endl;
+if(idade < 0){
+      std::cout<<"Idade inválida!"<<std::endl;
+}else if(idade <= 2){
+//else if = se não se
+      std::cout<<"Voce é um bebe!"<<std::endl;
+}else if(idade <= 8){
+      std::cout<<"Você é uma criança!"<<std::endl;
+}else if(idade <= 16){
+      std::cout<<"Voce é um(a) adolescente!"<<std::endl;
+}else if(idade <= 60){
+      std::cout<<"Voce é adulto(a)!"<<std::endl;
 }else{
-      std::cout<<"Voce é menor de idade!!"<<std::endl;
+      std::cout<<"Voce é um(a) senior!"<<std::endl;
 }
 
       return 0;
