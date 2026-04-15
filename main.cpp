e2c39520c57e1e739dccca87b970a02f4f701199
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-15 19:47:37 -0300

    sleep_for

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> //para definir unidades de tempo (ms, segundos)
 
 int main(){
 
       for(int i = 10; i >= 0; i--){
             std::cout<<"A bomba ira explodir em.. "<<i<<std::endl;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(200)); 
+            //tempo em que ocorre a contagem
+            //seconds => milliseconds
       }
 
+      
       return 0;
 }
