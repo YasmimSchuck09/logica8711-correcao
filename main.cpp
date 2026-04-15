d2872db84df7080cca7accca82f41f6dbeaf720d
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-15 19:14:22 -0300

    utilização do for

--- main.cpp
+++ main.cpp
@@ -1,32 +1,10 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-std::string nome;
-int idade;
-float altura;
-
-std::cout<<"Digite seu nome: "<<std::endl;
-std::cin>>nome;
-
-std::cout<<"Digite sua altura: "<<std::endl;
-std::cin>>altura;
-
-std::cout<<"Digite sua idade: "<<std::endl;
-std::cin>>idade;
-
-if(idade <= 10){
-      std::cout<<"Voce se encontra na classificação infantil!"<<std::endl;
-}else if(idade <= 18){
-      std::cout<<"Voce se encontra na classificação adolescente!"<<std::endl;
-}else if(idade <= 60){
-      std::cout<<"Voce se encontra na classificação adulta!"<<std::endl;
-}else{
-      std::cout<<"Voce se encontra na classificação Senior!"<<std::endl;
-}
-
-std::cout<<"Boa noite "<<" Yasmim!"<<std::endl;
+      for(int i = 1; i <= 10; i++){
+            std::cout<<"Este é um loop!!"<<std::endl;
+      }
 
       return 0;
 }
