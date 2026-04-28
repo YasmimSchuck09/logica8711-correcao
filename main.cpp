4f0656cb9826b558972e32c9f49094f2cfe63b67
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-28 20:25:25 -0300

    atividades com o void

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
-void myFunction(){ //void = vazio 
-      std::cout<<"Ola Mundo!!"<<std::endl;
+
+void verificarAprovacao(float nota){
+if(nota >= 7){
+      std::cout<<"Status: Aprovado!"<<std::endl;
+}else{
+      std::cout<<"Status: Reprovado!"<<std::endl;
 }
 
+}
 
 int main(){
 
-myFunction();
+      float minhanota = 8.5;
+
+      verificarAprovacao(minhanota);
 
       return 0;
 }
