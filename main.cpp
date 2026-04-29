48300fe6d5774c5bfcd613ebd192aae2e908235b
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-28 21:00:08 -0300

    void minhaFunção

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 
 
-void verificarAprovacao(float nota){
-if(nota >= 7){
-      std::cout<<"Status: Aprovado!"<<std::endl;
-}else{
-      std::cout<<"Status: Reprovado!"<<std::endl;
-}
+void minhaFuncao(std::string nome){
+      std::cout<<nome<<std::endl;
 
 }
 
 int main(){
 
-      float minhanota = 8.5;
-
-      verificarAprovacao(minhanota);
+      minhaFuncao("Yasmim");
+      minhaFuncao("Nome");
 
       return 0;
 }
