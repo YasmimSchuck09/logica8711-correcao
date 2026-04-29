f18b9fddcac0ebfa4a83e8059690bc5db4139d90
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-28 21:12:28 -0300

    void com a idade e nome

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
-
-void minhaFuncao(std::string nome){
-      std::cout<<nome<<std::endl;
-
+void minhaFuncao(std::string nome, int idade){
+      std::cout<<"Meu nome é: "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
+      
 
 int main(){
 
-      minhaFuncao("Yasmim");
-      minhaFuncao("Nome");
+      minhaFuncao("Yasmim", 16);
 
       return 0;
 }
