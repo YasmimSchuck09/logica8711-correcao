70d858ac6975d6334894b412f9a9404ecd177ed6
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-30 19:47:22 -0300

    exemplos if e else (transformação das termeparturas)

--- main.cpp
+++ main.cpp
@@ -3,25 +3,26 @@
 
 int main(){
 
-      int opcao;
-      std::string nome;
-      int idade;
+int choice;
+float temp, converted;
 
-      std::cout<<"---- Escolha a opcao adequada: "<<std::endl;
-      std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
-      std::cin>>opcao;
-
-      if(opcao ==1){
-            std::cout<<"Digite o nome: "<<std::endl;
-            std::cin>>nome;
-            std::cout<<"Digite sua idade: "<<std::endl;
-            std::cin>>idade;
-
-            std::cout<<"Seu nome e: "<<nome<<std::endl;
-            std::cout<<"Sua idade e: "<<idade<<std::endl;
-      }else{
-            std::cout<<"Ate a proxima!"<<std::endl;
-      }
-
+std::cout<<"------ Temperature converter ------"<<std::endl;
+std::cout<<"1: Celsius to Fahrenheit"<<std::endl;
+std::cout<<"2: Fahrenheit to Celsius"<<std::endl;
+std::cout<<"Choose an option (1 or 2): "<<std::endl;
+std::cin>>choice;
+if(choice == 1){
+      std::cout<<"Enter temperature in celsius: "<<std::endl;
+      std::cin>>temp;
+      converted = (temp * 1.8) + 32;
+      std::cout<<temp<<"C in "<<converted<<"F."<<std::endl;
+}else if(choice == 2){
+      std::cout<<"Enter tamperature in Fahrenheit: "<<std::endl;
+      std::cin>>temp;
+      converted = (temp - 32) / 1.8;
+      std::cout<<temp<<"F is"<<converted<<"C."<<std::endl;
+}else{
+      std::cout<<"Invalid Option! Please restart."<<std::endl;
+}
       return 0;
 }
