39763f6e1bb7f87addc58f9516800eef9df4b2d2
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-30 20:51:19 -0300

    atividade if e else

--- main.cpp
+++ main.cpp
@@ -3,30 +3,20 @@
 
 int main(){
 
-//blAlmount: valor em reais| exchangeRate: Taxa de Câmbio| usAlmount: Valor em dólares
-double brlAmount, exchangeRate, usdAmount;
-int choice;
+int num;
+float resultado;
 
-std::cout<<"---- Currency Converter: BRL to USD ------"<<std::endl;
+std::cout<<"Digite um valor para num: "<<std::endl;
+std::cin>>num;
 
-//Entreda de taxa de câmbio (quanto vale 1 dolar hoje)
-
-      std::cout<<"Enter de current exchange rate ( How much is 1 dollar in Reais?)"<<std::endl;
-      std::cin>>exchangeRate;
-
-      // Entrada da quantia em reaisque a pessoa deseja converter
-      std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
-      std::cin>>brlAmount;
-
-      //Verificação lógica: garante que o valor informado seja positivo 
-      if(brlAmount > 0){
-            //cálculo divide o montante em reais pela taxa de câmbio
-            usdAmount = brlAmount / exchangeRate;
-            std::cout<<"You will have $"<<usdAmount<<" US Dollars."<<std::endl;
-      }else{
-            //Mensagem de erro caso o vlaor seja zero ou negativo
-            std::cout<<"Error: Please enter a valid amount grater than zero."<<std::endl;
-      }
-
-      return 0;
+std::cout<<num%2<<std::endl;
+ if(num % 2 == 0){
+      std::cout<<"O numero digitado é par"<<std::endl;
+ }else if(num % 2 == 1){
+      std::cout<<"O numero digitado é impar"<<std::endl;
+ }else{
+      std::cout<<"Numero invalido"<<std::endl;
+ }
+ 
+  return 0;
 }
