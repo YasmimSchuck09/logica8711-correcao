9dd16e9cfcb85ee3782ea42159f46bdf04d4a545
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-04-30 19:20:17 -0300

    exemplificação de if e else para o cadastro

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
-
-int doubleGame(int x){
-      return x * 2;
-}
+#include<string>
 
 int main(){
 
-      for(int i = 1; i <= 5; i++){
-            std::cout<<"O dobro de "<<i<<" é "<<doubleGame(i)<<std::endl;
+      int opcao;
+      std::string nome;
+      int idade;
+
+      std::cout<<"---- Escolha a opcao adequada: "<<std::endl;
+      std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
+      std::cin>>opcao;
+
+      if(opcao ==1){
+            std::cout<<"Digite o nome: "<<std::endl;
+            std::cin>>nome;
+            std::cout<<"Digite sua idade: "<<std::endl;
+            std::cin>>idade;
+
+            std::cout<<"Seu nome e: "<<nome<<std::endl;
+            std::cout<<"Sua idade e: "<<idade<<std::endl;
+      }else{
+            std::cout<<"Ate a proxima!"<<std::endl;
       }
 
       return 0;
