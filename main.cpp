296b2d3732a1a3fa1835c2b60d838d05452c71a2
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-05-26 19:53:32 -0300

    long long

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
-#include<format>
-#include<string>
+
+long long calcularFatorial(int n){
+    long long resultado = 1;
+
+    for(int i = 1; i <=  n; i++){
+        resultado *= i;
+    }
+
+    return resultado; 
+}
 
 int main(){
+    int numero;
 
-    std::string nome = "Eduardo";
+    std::cout<<"Digite um numero para calcular fatorial: "<<std::endl;
+    std::cin>>numero;
 
-    std::string mensagem = std::format("Ola, {}! Voce tem {} anos. \n", nome);
-    std::cout<<mensagem;
-
-
-    return 0;
+    if(numero < 0){
+        std::cout<<"Erro: Fatorial de numero negativo nao existe!"<<std::endl;
+    }else{
+        long long fatorial = calcularFatorial(numero);
+        std::cout<<numero<<"! = "<<fatorial<<std::endl;
+    }
 }
+
+// este é o exemplo da ulização do long long quando precisamos calcular o valor fatorial: 
+// exemplo: 5 é o numero escohido, então ele precisa se multiplicar com o numero anterior: 5x4: 20
