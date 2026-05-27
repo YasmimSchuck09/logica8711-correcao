8633bed61539c778bf7b5025a406c6459d9042b7
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-05-26 21:06:42 -0300

    exercicio sobre o uso do for

--- main.cpp
+++ main.cpp
@@ -1,24 +1,15 @@
 #include<iostream>
 
-int quadrado(int n){
-    return n * n;
-}
-
-int cubo(int n){
-    return n * n * n;
-}
-
 int main(){
 
-    std::cout<<"Quadrado de 5: "<<quadrado(5)<<std::endl;
-    std::cout<<"Quadrado de 8: "<<quadrado(8)<<std::endl;
-    std::cout<<"Quadrado de 10: "<<quadrado(10)<<std::endl;
+    int numeros[5];
+    int soma = 0;
 
-
-    std::cout<<"Cubo de 2: "<<cubo(2)<<std::endl;
-    std::cout<<"Cubo de 5: "<<cubo(5)<<std::endl;
-    std::cout<<"Cubo de 3: "<<cubo(3)<<std::endl;
-    
-
-    return 0;
+    for(int i = 0; i <= 4; i++){
+        std::cout<<"Digite o numero: "<<(i + 1)<<": ";
+        std::cin>>numeros[i];
+        soma += numeros[i];
 }
+    std::cout<<"Soma: "<<soma<<std::endl;
+}
+
