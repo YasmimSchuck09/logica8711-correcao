123f82fae37589c15e862ed4e808df586ca21e52
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-06-09 21:07:00 -0300

    sistema ID, produto e preço com struct

--- main.cpp
+++ main.cpp
@@ -1,67 +1,58 @@
 #include<iostream>
 #include<string>
 
-struct Tarefa{
-    int id; // numero identificador 
-    std::string descricao; // texto
-    bool concluida; // trará a resposta como true or false 
+struct Loja{
+    int id;
+    std::string descricaoProduto;
+    float preco;
+    bool concluida;
 
 };
-Tarefa tarefas[50]; // aqui eu puxei a struct (podendo ter ate 50 tarefas nela)
-int totalTarefas = 0; // aqui eu digo que começa a partir do 0
+Loja PRODUTO[50]; 
+int totalProdutos;
+
 
 void adicionar(){
-    std::cout<<"\n --- Adicionar tarefas ---"<<std::endl; // título
+    std::cout<<"--- Adicionar produtos ---"<<std::endl;
 
-    std::cout<<"ID: ";
-    std::cin>>tarefas[totalTarefas].id; // ID da tarefa
+   std::cout<<"ID: ";
+   std::cin>>PRODUTO[totalProdutos].id;
+
+   std::cout<<"Preco: ";
+   std::cin>>PRODUTO[totalProdutos].preco;
 
     std::cin.ignore();
-    std::cout<<"Descricao: ";
-    std::getline(std::cin, tarefas[totalTarefas].descricao);
+    std::cout<<"Produto: ";
+    std::getline(std::cin, PRODUTO[totalProdutos].descricaoProduto);
 
-    tarefas[totalTarefas].concluida = false;
+    PRODUTO[totalProdutos].concluida = false;
 
-    totalTarefas++;
-    std::cout<<"Tarefa adicionada!"<<std::endl;
+    totalProdutos++;
+    std::cout<<"Produto adicionado!"<<std::endl;
 }
 
-    void listar(){
-        if(totalTarefas == 0){
-            std::cout<<"\nNenhuma tarefa!"<<std::endl;
+
+    void exibir(){
+        if(totalProdutos == 0){
+            std::cout<<"\nNenhum produto!"<<std::endl;
             return;
         }
-        std::cout<<"\n--- Tarefas ---"<<std::endl;
-        for(int i = 0; i < totalTarefas; i++){
-            std::string status = tarefas[i].concluida ? "S" : "N";
-            std::cout<<"["<<status<<"]"<<tarefas[i].id<<" -  "<<tarefas[i].descricao<<std::endl;
+        std::cout<<"\n--- Produtos ---"<<std::endl;
+        for(int i = 0; i < totalProdutos; i++){
+            std::string status = PRODUTO[i].concluida ? "S" : "N";
+            std::cout<<"["<<status<<"]"<<PRODUTO[i].id<<" - "<<PRODUTO[i].descricaoProduto<<std::endl;
         }
     }
-
-        void marcarConcluida(){
-            int idBuscado;
-            std::cout<<"\nID da tarefa: ";
-            std::cin>>idBuscado;
-
-            for(int i = 0;i< totalTarefas; i++){
-                if(tarefas[i].id == idBuscado){
-                    tarefas[i].concluida = true;
-                    std::cout<<"Marcada como concluida!"<<std::endl;
-                    return;
-                }
-            }
-            std::cout<<"Tarefa nao encontrada!"<<std::endl;
-        }
 
         void menu(){
             std::cout<<"\n === TODO LIST ==="<<std::endl;
             std::cout<<"1. Adicionar"<<std::endl;
-            std::cout<<"2. Listar"<<std::endl;
-            std::cout<<"3. Marcar concluida"<<std::endl;
-            std::cout<<"4. Sair"<<std::endl;
+            std::cout<<"2. Exibir"<<std::endl;
+            std::cout<<"3. Sair"<<std::endl;
             std::cout<<"Escolha: "<<std::endl;
         }
         int main(){
+            menu();
             int opcao;
             
             while(true){
@@ -73,15 +64,12 @@ void adicionar(){
                         adicionar();
                         break;
                     case 2:
-                        listar();
+                        exibir();
                         break;
                     case 3: 
-                        marcarConcluida();
-                        break;
-                    case 4:
                         std::cout<<"Ate logo!"<<std::endl;
                         return 0;
-                    defaul:
+                    default: 
                         std::cout<<"Opcao invalida!"<<std::endl;
                 }
             }
