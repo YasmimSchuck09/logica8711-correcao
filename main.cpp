49295137ea7cfd42c8c541e9bab8d9cb66835a43
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-06-11 19:29:40 -0300

    torre de Hanoi

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include<string>
+
+void torredeHanoi(int n, std::string origem, std::string destino, std::string auxiliar){
+    if(n == 1){
+            std::cout<<"Mover disco "<<n<<" de "<<origem<<" para "<<destino<<std::endl;
+            return;
+    }
+    torredeHanoi(n - 1, origem, auxiliar, destino);
+    std::cout<<"Mover disco "<<n<<" de "<<origem<<" para "<<destino<<std::endl;
+    torredeHanoi(n - 1, auxiliar,destino, origem);
+}
 
 int main(){
-    int idade = 17;
+    int numDiscos;
+    std::cout<<"Quantos discos: ";
+    std::cin>>numDiscos;
 
-    std::string resultado = (idade >= 18) ? "Maior d eidade!" : "Menor de idade";
+    std::cout<<"\nSequencia de movimentos: "<<std::endl;
+    torredeHanoi(numDiscos, "A", "C", "B");
 
-    std::cout<<resultado<<std::endl;
+    std::cout<<"\nTotal de movimentos: "<<(1 << numDiscos) - 1<<std::endl;
+
+    return 0;
 }
