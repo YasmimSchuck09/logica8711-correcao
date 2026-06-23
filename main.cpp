031f72b728a4f6fb91f55eeb9b2f7ec2b323d873
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-06-23 20:27:55 -0300

    array - determinando par e impar

--- main.cpp
+++ main.cpp
@@ -2,28 +2,24 @@
 
 int main(){
 
-int numeros[] = {1,2,3,4,5,3,6};
-int tamanho = 8;
+int numeros[] = {1,2,3,4,5,6,7,8,9,10};
+int tamanho;
 
-std::cout<<"===== NUMEROS DUPLICADOS ======"<<std::endl;
+std::cout<<"==== PARES E IMPARES ====="<<std::endl;
 std::cout<<std::endl;
 
-std::cout<<"Array:> ";
+std::cout<<"Pares: ";
 for(int i = 0; i < tamanho; i++){
-    std::cout<<numeros[i]<<" ";
-}
-    std::cout<<std::endl;
-
-    std::cout<<"Numeros duplicados: "<<std::endl;
-    for(int i = 0; i <tamanho; i++){
-        for(int j = i + 1; j < tamanho; j++){
-            if(numeros[i] == numeros[j]){
-                std::cout<<numeros[i]<<" aparece mais de uma vez!"<<std::endl;
-                break;
-            }
-        }
+    if(numeros[i] % 2 == 0){
+        std::cout<<numeros[i]<<" ";
     }
-
+}
+std::cout<<std::endl;
+for(int i = 0; i < tamanho; i++){
+    if(numeros[i] % 2 != 0){
+        std::cout<<numeros[i]<<" ";
+    }
+}
     return 0;
 }
 
