6ce4cd68865362c2588d54bb0d5431c4cdb15e79
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-06-30 21:32:06 -0300

    Encontrar maior elemento

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 
-// 2. Soma simples
-// Leia 5 números e calcule a soma total.
+// 3. Encontrar maior
+// Leia 5 números e encontre o maior deles.
 
-int main(){
-    int numeros[5];
-    int soma = 0;
-    for(int i = 0; i < 5; i++){
-        std::cout<<"Digite um numero: "<<std::endl;
-        std::cin>>numeros[i];
-        soma += numeros[i];
+int encontrarMaior(int* arr, int tamanho)
+//arr = o vetor de numeros
+//tamnho = quantidade de elementos do vetor
+{
+    int maior = arr[0]; // como não sabemos qual é o maior, adicionamos o primeiro como maior
+    for(int i = 1; i < tamanho; i++){ // aqui quer dizer que ele vai percorrer pelos outros numeros
+        if(arr[i] > maior){
+            maior = arr[i];
+        }
     }
-    std::cout<<"A soma dos numeros é: "<<soma<<std::endl;
+    return maior;
+}
+int main(){
+    int arr[] = {3, 7, 2, 9, 1};
+    std::cout<<"Maior elemento: "<<encontrarMaior(arr, 5)<<std::endl;
     return 0;
 }
-
