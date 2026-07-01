2ecfe1b0c576ac66598c5c44ef084d362b4bf8de
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-06-30 21:31:02 -0300

    Soma de valores

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 
- int main(){
-    int numeros[5] = {5, 10, 15, 2, 12};
-    	std::cout<<"Numeros: "<<std::endl;
+// 2. Soma simples
+// Leia 5 números e calcule a soma total.
+
+int main(){
+    int numeros[5];
+    int soma = 0;
     for(int i = 0; i < 5; i++){
-        std::cout<<numeros[i]<<" ";
+        std::cout<<"Digite um numero: "<<std::endl;
+        std::cin>>numeros[i];
+        soma += numeros[i];
     }
+    std::cout<<"A soma dos numeros é: "<<soma<<std::endl;
     return 0;
 }
+
