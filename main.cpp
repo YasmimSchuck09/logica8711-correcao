2a17a74af73dc1f8a09e9415e77aa937274497ae
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-07-02 21:19:38 -0300

    Remover um elemento especifico

--- main.cpp
+++ main.cpp
@@ -5,25 +5,25 @@ int main(){
 
 std::vector<int>numeros; // variaveis
 
-std::cout<<"===== Acessar elemento ====="<<std::endl;
+std::cout<<"===== Remover elemento especifico ====="<<std::endl;
 
-numeros.push_back(100); // determinar valores 
-numeros.push_back(200);
-numeros.push_back(300);
-numeros.push_back(400);
+numeros.push_back(10);
+numeros.push_back(20);
+numeros.push_back(30);
+numeros.push_back(40);
 
-std::cout<<"Vector: ";
-for(int i = 0; i < numeros.size(); i++){ // impressão dos valores 
+std::cout<<"Antes: ";
+for(int i = 0; i < numeros.size(); i++){
     std::cout<<numeros[i]<<" ";
 }
 std::cout<<std::endl;
 
-std::cout<<"Elemento na posicao 0: "<<numeros[0]<<std::endl; // numeros nos locais determinados 
-std::cout<<"Elemento na posicao 2: "<<numeros[2]<<std::endl;
-std::cout<<"Ultimo elemento: "<<numeros[numeros.size() - 1]<<std::endl;
-// numeros.size() = representam os 4 numeros que foram determinados em cima 
-// como tem 4 numeros  (4- 1) = 3, ou seja a posicão 3(400)
+numeros.erase(numeros.begin() + 2);
 
+std::cout<<"Depois: ";
+for(int i = 0; i < numeros.size(); i++){
+    std::cout<<numeros[i]<<" ";
+}
         return 0;
 }
 
