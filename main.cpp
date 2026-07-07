d12eea82913161da7de3cde896d2021268c798e6
YASMIN GOMES SCHUCK <04925039075@senacrs.edu.br>
2026-07-07 19:48:34 -0300

    encontrar a posição do numero específico com vector

--- main.cpp
+++ main.cpp
@@ -5,29 +5,32 @@ int main(){
 
 std::vector<int>numeros; // variaveis
 
-std::cout<<"Contar ocorrencias: "<<std::endl;
+std::cout<<"Buscar elemento: "<<std::endl;
 
-numeros.push_back(5);
-numeros.push_back(10);
-numeros.push_back(5);
-numeros.push_back(15);
-numeros.push_back(15);
-numeros.push_back(5);
-numeros.push_back(15);
+numeros.push_back(100);
+numeros.push_back(200);
+numeros.push_back(300);
+numeros.push_back(400);
 
 std::cout<<"Vector: ";
 for(int i = 0; i < numeros.size(); i++){
     std::cout<<numeros[i]<<" "; // aqui estou mostrando todos os numeros que foram determinados acima
 }
 
-int contador = 0;
-for(int i = 0; i < numeros.size(); i++){ // aqui o contador começa no 0 e vai até a ultima posição
-    if(numeros[i] == 5){  //se os numeros citados acima forem iguais a 5, CONTA QUANTOS TEM 
-        contador++;
+int buscado = 300;
+int posicao = -1;
+
+for(int i = 0; i < numeros.size(); i++){ 
+    if(numeros[i] == buscado){ //se um dos numeros for igual a 300, mostrar sua posição
+        posicao = i;
+        break;
     }
 }
-std::cout<<"\nO numero 5 aparece: "<<contador<<" vezes!"<<std::endl;
-
+if(posicao != -1){ 
+    std::cout<<"\nNumero "<<buscado<<" encontrado na posicao "<<posicao<<std::endl;
+}else{
+    std::cout<<"\nNumero não encontrado!"<<std::endl;
+}
         return 0;
 }
 
